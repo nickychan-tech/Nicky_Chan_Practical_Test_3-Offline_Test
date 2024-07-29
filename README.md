88308a85856bd5b39744ec1c5c06f58a67565dcf
Nicky <nickychan.cck@gmail.com>
2024-07-29 22:22:41 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Nicky_Chan_Practical_Test_3-Offline_Test
+ 
---
+++ index.js
@@ -0,0 +1,177 @@
+const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTkwNDIzNDY3ODZhOTIxY2E3MzJmZmM0YzAxYTIxZiIsIm5iZiI6MTcyMTgwNzc3MC40NTAyNzcsInN1YiI6IjY2YTBhN2VhZjdhMTE0YTA4M2UwZDRhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SQjgB9waWCim1CPQFSbrHehebcAcr4uDQV8iIYgIrps';
+const fadeSpeed = 400;
+
+const mainMenuAnimLen = 3;
+let mainMenuAnimCount = mainMenuAnimLen;
+
+const hasTouch = isTouchDevice();
+let results = [];
+var body, loading;
+
+function homeInit() {
+	body = $('body');
+	loading = $('.loading');
+
+	if(!hasTouch) body.addClass('noTouch')
+
+	bindHomeEvent();
+	getMovieList();
+}
+
+function bindHomeEvent() {
+	$('.btn-sort').on('click', function(e) {
+		e.preventDefault();
+		$(this).toggleClass('by-popularity');
+
+		if($(this).hasClass('by-popularity')) {
+			let _resultsCopy = [...results];
+			_resultsCopy.sort((a, b) => {
+				if(a.popularity < b.popularity) {
+					return 1;
+				}
+				if(a.popularity > b.popularity) {
+					return -1;
+				}
+				return 0;
+			});
+			createMovieList(_resultsCopy)
+
+		} else {
+			createMovieList(results)
+
+		}
+	});
+
+	// Header menu
+	$('.btn-open-main_menu').on('click', function(e) {
+		e.preventDefault();
+		if(body.hasClass('menu-opened')) {
+			closeMainMenu();
+		} else {
+			openMainMenu();
+		}
+	});
+	$('.btn-close-main_menu').on("click", function(e) {
+		e.preventDefault();
+		closeMainMenu();
+	});
+
+	$('.nav-wrapper').on("click", function(e) {
+		const eTarget = $(e.target);
+		if(!eTarget.hasClass('nav-container') && !eTarget.parents('.nav-wrapper').length) {
+			e.preventDefault();
+			console.log(eTarget)
+			closeMainMenu();
+		}
+	});
+	$('.nav-wrapper').on("animationend webkitAnimationEnd", function(e) {
+		e.preventDefault();
+		if(--mainMenuAnimCount <= 0) {
+			if(body.hasClass('menu-opened menu-closing')) {
+				body.removeClass('menu-opened menu-closing');
+			}
+		}
+	});
+}
+
+// Main Menu
+function openMainMenu() {
+	mainMenuAnimCount = mainMenuAnimLen;
+	body.addClass('menu-opened');
+}
+
+function closeMainMenu() {
+	mainMenuAnimCount = mainMenuAnimLen;
+	if(body.hasClass('menu-opened')) {
+		body.addClass('menu-closing');
+	}
+}
+
+function createMovieList(data) {
+	let _imgBaseUrl = 'https://image.tmdb.org/t/p/w400/';
+	let _html = '';
+	for(let i=0; i<data.length; i++) {
+		let _item = data[i];
+		_html += `<div class="main-item" data-popularity="${_item.popularity}">
+			<div class="item-img-container">
+				<img src="${_imgBaseUrl + _item.poster_path}" alt="" class="item-img">
+			</div>
+			<div class="item-content">
+				<div class="item-title">${_item.title}</div>
+				<div class="item-desc">${_item.overview}</div>
+				<div class="item-popularity">${formatMoney(_item.popularity, 0)}</div>
+			</div>
+		</div>`;
+	}
+	$('.main-list').html(_html);
+}
+
+// call API
+function getMovieList() {
+	showLoading();
+
+	const options = {
+		method: 'GET',
+		headers: {
+			accept: 'application/json',
+			Authorization: 'Bearer ' + apiKey
+		}
+	};
+	
+	fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+	.then(response => response.json())
+	.then(response => {
+		results = response.results;
+		createMovieList(results);
+		hideLoading();
+	})
+	.catch(err => {
+		console.error(err);
+		hideLoading();
+	});
+}
+
+// adult: false
+// backdrop_path: "/tncbMvfV0V07UZozXdBEq4Wu9HH.jpg"
+// genre_ids: (4) [28, 80, 53, 35]
+// id: 573435
+// original_language: "en"
+// original_title: "Bad Boys: Ride or Die"
+// overview: "After their late former Captain is framed, Lowrey and Burnett try to clear his name, only to end up on the run themselves."
+// popularity: 9457.863
+// poster_path: "/nP6RliHjxsz4irTKsxe8FRhKZYl.jpg"
+// release_date: "2024-06-05"
+// title: "Bad Boys: Ride or Die"
+// video: false
+// vote_average: 7.647
+// vote_count: 1079
+
+// Loading
+function showLoading() {
+	loading.fadeIn(fadeSpeed);
+}
+
+function hideLoading() {
+	loading.fadeOut(fadeSpeed);
+}
+
+// Helpers
+function isTouchDevice() {
+	return (('ontouchstart' in window) ||
+	   (navigator.maxTouchPoints > 0) ||
+	   (navigator.msMaxTouchPoints > 0));
+}
+
+function formatMoney(number, decPlaces, decSep, thouSep) {
+    decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
+    decSep = typeof decSep === "undefined" ? "." : decSep;
+    thouSep = typeof thouSep === "undefined" ? "," : thouSep;
+    var sign = number < 0 ? "-" : "";
+    var i = String(parseInt(number = Math.abs(Number(number) || 0).toFixed(decPlaces)));
+    var j = (j = i.length) > 3 ? j % 3 : 0;
+
+    return sign +
+        (j ? i.substr(0, j) + thouSep : "") +
+        i.substr(j).replace(/(\decSep{3})(?=\decSep)/g, "$1" + thouSep) +
+        (decPlaces ? decSep + Math.abs(number - i).toFixed(decPlaces).slice(2) : "");
+}
